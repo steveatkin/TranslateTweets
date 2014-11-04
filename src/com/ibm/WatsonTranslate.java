ade3f3eb73bf11d9b5196f48c15f3494fcd132e3
Administrator <admin@local.host>
2014-11-04 17:00:39 -0500

    Added lang service

--- src/com/ibm/WatsonTranslate.java
+++ src/com/ibm/WatsonTranslate.java
@@ -21,39 +21,64 @@ import org.apache.http.message.BasicNameValuePair;
 public class WatsonTranslate {
 	private static Logger logger = Logger.getLogger(WatsonTranslate.class.getName());
 
-	private static String serviceName = "machine_translation";
-	
-	// If running locally complete the variables below with the information in VCAP_SERVICES
-	private static String baseURL = "https://gateway.watsonplatform.net/laser/service/api/v1/smt/9c7376c0-24c7-4cf6-9194-baad1c2d2061";
-	private static String username = "6e9ade17-a5c9-4ecc-98cb-18e51656feda";
-	private static String password = "1bUlwNuOdUBB";
-	
+	private static String translationService = "machine_translation";
+	private static String languageService = "language_identification";
+
+	// If running locally add the information from VCAP_SERVICES
+	private static String baseURLTranslation = "put url here";
+	private static String usernameTranslation = "put username here";
+	private static String passwordTranslation = "put password here";
+
+	private static String baseURLLanguage = "put url here";
+	private static String usernameLanguage = "put username here";
+	private static String passwordLanguage = "put password here";
+
 	static {
 		processVCAP_Services();
 	}
-	
+
 	private static void processVCAP_Services() {
     	logger.info("Processing VCAP_SERVICES");
         JSONObject sysEnv = getVcapServices();
         if (sysEnv == null) return;
-        logger.info("Looking for: "+ serviceName );
-        
-        if (sysEnv.containsKey(serviceName)) {
-			JSONArray services = (JSONArray)sysEnv.get(serviceName);
-			JSONObject service = (JSONObject)services.get(0);
-			JSONObject credentials = (JSONObject)service.get("credentials");
-			baseURL = (String)credentials.get("url");
-			username = (String)credentials.get("username");
-			password = (String)credentials.get("password");
-			logger.info("baseURL  = "+baseURL);
-			logger.info("username   = "+username);
-			logger.info("password = "+password);
-    	} else {
-        	logger.warning(serviceName + " is not available in VCAP_SERVICES, "
+
+
+				logger.info("Looking for: "+ translationService);
+
+        if (sysEnv.containsKey(translationService)) {
+					JSONArray services = (JSONArray)sysEnv.get(translationService);
+					JSONObject service = (JSONObject)services.get(0);
+					JSONObject credentials = (JSONObject)service.get("credentials");
+					baseURLTranslation = (String)credentials.get("url");
+					usernameTranslation = (String)credentials.get("username");
+					passwordTranslation = (String)credentials.get("password");
+					logger.info("baseURL  = "+baseURLTranslation);
+					logger.info("username   = "+usernameTranslation);
+					logger.info("password = "+passwordTranslation);
+    		} else {
+        	logger.warning(translationService + " is not available in VCAP_SERVICES, "
         			+ "please bind the service to your application");
         }
+
+				logger.info("Looking for: "+ languageService);
+
+				if (sysEnv.containsKey(languageService)) {
+					JSONArray services = (JSONArray)sysEnv.get(languageService);
+					JSONObject service = (JSONObject)services.get(0);
+					JSONObject credentials = (JSONObject)service.get("credentials");
+					baseURLLanguage = (String)credentials.get("url");
+					usernameLanguage = (String)credentials.get("username");
+					passwordLanguage = (String)credentials.get("password");
+					logger.info("baseURL  = "+baseURLLanguage);
+					logger.info("username   = "+usernameLanguage);
+					logger.info("password = "+passwordLanguage);
+				} else {
+					logger.warning(languageService + " is not available in VCAP_SERVICES, "
+							+ "please bind the service to your application");
+				}
+
     }
-	
+
 	private static JSONObject getVcapServices() {
         String envServices = System.getenv("VCAP_SERVICES");
         if (envServices == null) return null;
@@ -66,30 +91,58 @@ public class WatsonTranslate {
         }
         return sysEnv;
     }
-	
+
 	public String translate(String text, String sid) {
 		String response = "";
 		List<NameValuePair> qparams = new ArrayList<NameValuePair>();
 		qparams.add(new BasicNameValuePair("txt",text ));
 		qparams.add(new BasicNameValuePair("sid",sid ));
 		qparams.add(new BasicNameValuePair("rt","text" ));
-		
+
 		try {
 			Executor executor = Executor.newInstance();
-    		URI serviceURI = new URI(baseURL).normalize();
-    	    String auth = username + ":" + password;
+    		URI serviceURI = new URI(baseURLTranslation).normalize();
+    	    String auth = usernameTranslation + ":" + passwordTranslation;
     	    byte[] responseB = executor.execute(Request.Post(serviceURI)
 			    .addHeader("Authorization", "Basic "+ Base64.encodeBase64String(auth.getBytes()))
-			    .bodyString(URLEncodedUtils.format(qparams, "utf-8"), 
+			    .bodyString(URLEncodedUtils.format(qparams, "utf-8"),
 			    		ContentType.APPLICATION_FORM_URLENCODED)
 			    ).returnContent().asBytes();
 
     	    response = new String (responseB,"UTF-8");
 		}
 		catch(Exception e) {
-			logger.log(Level.SEVERE, "got error: "+e.getMessage(), e);
+			logger.log(Level.SEVERE, "Watson error: "+e.getMessage(), e);
 		}
-		
+
+		return response;
+	}
+
+
+
+	public String identify(String text) {
+		String response = "";
+		List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+		qparams.add(new BasicNameValuePair("txt",text));
+		qparams.add(new BasicNameValuePair("sid","lid-generic"));
+		qparams.add(new BasicNameValuePair("rt","text"));
+
+		try {
+			Executor executor = Executor.newInstance().auth(usernameLanguage, passwordLanguage);
+				URI serviceURI = new URI(baseURLLanguage).normalize();
+					String auth = usernameLanguage + ":" + passwordLanguage;
+					byte[] responseB = executor.execute(Request.Post(serviceURI)
+					.addHeader("Authorization", "Basic "+ Base64.encodeBase64String(auth.getBytes()))
+					.bodyString(URLEncodedUtils.format(qparams, "utf-8"),
+							ContentType.APPLICATION_FORM_URLENCODED)
+					).returnContent().asBytes();
+
+					response = new String (responseB,"UTF-8");
+		}
+		catch(Exception e) {
+			logger.log(Level.SEVERE, "Watson error: "+e.getMessage(), e);
+		}
+
 		return response;
 	}
 }
