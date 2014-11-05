57e483a6f82a6c76bfbc630daa943505f12e3e4f
Administrator <admin@local.host>
2014-11-05 12:26:26 -0500

    Added Watson lang support

--- src/com/ibm/WatsonTranslate.java
+++ src/com/ibm/WatsonTranslate.java
@@ -123,7 +123,7 @@ public class WatsonTranslate {
 	public String identify(String text) {
 		String language = "";
 		List<NameValuePair> qparams = new ArrayList<NameValuePair>();
-		qparams.add(new BasicNameValuePair("txt","This is a test of the service"));
+		qparams.add(new BasicNameValuePair("txt",text));
 		qparams.add(new BasicNameValuePair("sid","lid-generic"));
 		qparams.add(new BasicNameValuePair("rt","json"));
 
@@ -144,7 +144,6 @@ public class WatsonTranslate {
 			logger.log(Level.SEVERE, "Watson error: "+e.getMessage(), e);
 		}
 
-		logger.info("JSON Response: " + language);
 		return language;
 	}
 }
